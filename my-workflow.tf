66b877207a8b37bde0279ce5f03b4521dc8ebb03
mahesh.jadhav <jadhavmahesh4791@gmail.com>
2025-12-12 15:59:20 +0530

    my-workflow-2

---
+++ my-workflow.tf
@@ -0,0 +1,34 @@
+# backend.tf
+terraform {
+  required_version = ">= 1.4.0"
+  
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "my-workflow-terraform-state-bucket"
+    key            = "prod/terraform.tfstate"
+    region         = "ap-south-1"
+    encrypt        = true
+    dynamodb_table = "terraform-locks"
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+
+  assume_role {
+    role_arn = "arn:aws:iam::408190482558:role/my-master-role"
+  }
+
+  default_tags {
+    tags = {
+      Environment = "production"
+      ManagedBy   = "Terraform"
+    }
+  }
+}
