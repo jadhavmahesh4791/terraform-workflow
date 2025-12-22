9aad44aa88dd01c6ed1c9380eb379a923fb587bd
Mahesh S Jadhav <80886867+jadhavmahesh4791@users.noreply.github.com>
2025-12-22 18:07:35 +0530

    updating-s3-bucket-name

--- my-workflow.tf
+++ my-workflow.tf
@@ -10,7 +10,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "my-workflow-terraform-state-bucket"
+    bucket         = "my-tf-state-git-workflow"
     key            = "prod/terraform.tfstate"
     region         = "ap-south-1"
     // encrypt        = true
