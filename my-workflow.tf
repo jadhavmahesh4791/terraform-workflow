45d43dc069500189ac06497be22d21d83ddbfb66
Mahesh S Jadhav <80886867+jadhavmahesh4791@users.noreply.github.com>
2025-12-23 13:36:27 +0530

    final fix
    final fix

--- my-workflow.tf
+++ my-workflow.tf
@@ -13,8 +13,8 @@ terraform {
     bucket         = "my-tf-state-git-workflow"
     key            = "prod/terraform.tfstate"
     region         = "ap-south-1"
-    // encrypt        = true
-    // dynamodb_table = "terraform-locks"
+    encrypt        = true
+    dynamodb_table = "terraform-locks"
   }
 }
 
