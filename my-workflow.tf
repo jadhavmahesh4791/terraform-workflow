8c6cdcac463c6f87ba5407cd1f401f38ef889e6a
jadhavmahesh4791 <80886867+jadhavmahesh4791@users.noreply.github.com>
2026-03-01 13:15:20 +0000

    style: auto-fix terraform formatting

--- my-workflow.tf
+++ my-workflow.tf
@@ -10,10 +10,10 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "my-tf-state-git-workflow-1"
-    key            = "prod/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
+    bucket  = "my-tf-state-git-workflow-1"
+    key     = "prod/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
   }
 }
 
